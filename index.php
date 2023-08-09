ef47f584d1ba03edb0edc4e91350c061535659d3
MAIGA FAICAL <faicalmaiga78@gmail.com>
2023-08-09 12:53:33 +0000

    ajout dossiers par FAICAL

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annuaire des apprenants</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <section class="container-fluid mt-2">
+        <nav class="navbar navbar-light bg-danger">
+            <form class="container-fluid justify-content-start">
+                <div class="col-lg-3">
+                    <img src="Images/logo simplon.png" alt class="w-50" style="margin-left: 50px;">
+                </div>
+                <div style="margin-right: 220px;">
+                    <button class="btn btn-outline-dark btn btn-light  me-5" type="button">ACCUEIL</button>
+                    <button class="btn btn-outline-dark btn btn-light  me-5" type="button">APPRENANTS</button>
+                    <button class="btn btn-outline-dark btn btn-light  me-5" type="button">TUTEURS
+                    </button>
+                </div>
+                <div class="col-lg-3">
+                    <img src="Images/logo hcr 222.png" alt class="w-75 ">
+                </div>
+    </section>
+    <h2 class="text-center">BIENVENUE DANS GESTION DES APPRENANTS</h2>
+    <section class=" contenair p-2 bg-danger mt-3 m-3"> 
+        <div>
+        <img src="Images/IMAGE FORMATION1.jpeg" alt="" style="height: 350px; width:100%">
+        </div>
+    </section>
+    <section class="bg-danger text-light text-center m-3">
+        <h3>COPYRIGHT GROUPE OMEGA 2023</h3>
+    </section>
+
+</body>
+
+</html>
